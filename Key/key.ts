e1b693f7a6b7e53b6635ad632ee03f5227763743
aleksandrbaskin <solohinalex1@gmail.com>
2023-02-22 13:43:56 +0200

    init

---
+++ Key/key.ts
@@ -0,0 +1,27 @@
+export const mnemonic =
+    "client " +
+    "town " +
+    "culture " +
+    "asset " +
+    "flat " +
+    "husband " +
+    "reflect " +
+    "dinner " +
+    "raven " +
+    "matrix " +
+    "festival " +
+    "list " +
+    "camp " +
+    "lobster " +
+    "knife " +
+    "nation " +
+    "safe " +
+    "glad " +
+    "reform " +
+    "exile " +
+    "thank " +
+    "common " +
+    "high " +
+    "diary";
+
+export const cellPath = "escrow.cell";
--- scripts/deployEscrow.ts
+++ scripts/deployEscrow.ts
@@ -11,3 +11,52 @@ export async function run(provider: NetworkProvider) {
 
     // run methods on `openedContract`
 }
+
+import { Contract, ContractProvider, Sender, Address, Cell, contractAddress, beginCell } from "ton-core";
+
+
+// 1024 max to cell send
+export class SaveDataToCell implements Contract {
+
+    static createForDeploy(code: Cell, initialCounterValue: number, bits: number): MainContract {
+        const data = beginCell()
+            .storeUint(initialCounterValue, bits)
+            .endCell();
+        const workchain = 0;
+        const address = contractAddress(workchain, { code, data });
+        return new MainContract(address, { code, data });
+    }
+
+
+    async sendDeploy(provider: ContractProvider, via: Sender) {
+        await provider.internal(via, {
+            value: "1.01",
+            bounce: false
+        });
+    }
+
+    constructor(readonly address: Address, readonly init?: { code: Cell, data: Cell }) {}
+}
+
+export default class MainContract implements Contract {
+
+    static createForDeploy(code: Cell, firstSigna: number, secondSigna: number): MainContract {
+        const data = beginCell()
+            .storeUint(firstSigna, 512)
+            .storeUint(secondSigna, 512)
+            .endCell();
+        const workchain = 0;
+        const address = contractAddress(workchain, { code, data });
+        return new MainContract(address, { code, data });
+    }
+
+
+    async sendDeploy(provider: ContractProvider, via: Sender) {
+        await provider.internal(via, {
+            value: "1.01",
+            bounce: false
+        });
+    }
+
+    constructor(readonly address: Address, readonly init?: { code: Cell, data: Cell }) {}
+}
